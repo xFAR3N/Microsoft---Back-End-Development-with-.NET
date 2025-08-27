83e8afd1ecbbd75298b72051155f38f24904fea3
Damian Zabagło <damian.zabaglo@microsoft.wsei.edu.pl>
2025-08-27 21:24:43 +0200

    Dodaj pliki projektów.

---
+++ Microsoft - Back-End Development with .NET/Program.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+namespace Microsoft___Back_End_Development_with_.NET
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public List<string> Tags { get; set; }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = "{\"Name\": \"Laptop\", \"Price\": 999.99, \"Tags\": [\"Electronics\", \"Computers\"]}";
+            
+        }
+    }
+}
