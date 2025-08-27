a8909b06ee2b826bada156166fd70d12f8862268
Damian Zabagło <damian.zabaglo@microsoft.wsei.edu.pl>
2025-08-27 21:40:25 +0200

    Added Models/Product.cs

---
+++ Creating and Exploring ASP.NET Core Web API/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Creating_and_Exploring_ASP.NET_Core_Web_API.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+    }
+}
