8f62484a8025617a1484bc296c17b12eb7623c0f
Damian Zabagło <damian.zabaglo@microsoft.wsei.edu.pl>
2025-09-07 16:54:03 +0200

    Add new project, add class Person

---
+++ Serialization/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Serialization
+{
+    internal class Program
+    {
+        public class Person
+        {
+            public string UserName { get; set; }
+            public int UserAge { get; set; }
+        }
+            static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
