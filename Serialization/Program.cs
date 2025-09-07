5e418c84693d1505c29bf6ceba65e95b68e6d8df
Damian Zabagło <damian.zabaglo@microsoft.wsei.edu.pl>
2025-09-07 17:02:28 +0200

    Add serialization: Binary, JSON, XML

--- Serialization/Program.cs
+++ Serialization/Program.cs
@@ -1,15 +1,40 @@
-﻿namespace Serialization
+﻿using System.IO;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace Serialization
 {
-    internal class Program
+    public class Program
     {
         public class Person
         {
             public string UserName { get; set; }
             public int UserAge { get; set; }
         }
-            static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Person samplePerson = new Person { UserName = "Alice", UserAge = 30 };
+
+            using (FileStream fs = new FileStream("person.dat", FileMode.Create))
+            {
+                BinaryWriter writer = new BinaryWriter(fs);
+                writer.Write(samplePerson.UserName);
+                writer.Write(samplePerson.UserAge);
+            }
+            Console.WriteLine("Serialization complete.");
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Person));
+
+            using (StreamWriter writer = new StreamWriter("person.xml"))
+            {
+                xmlSerializer.Serialize(writer, samplePerson);
+            }
+
+            string jsonString = JsonSerializer.Serialize(samplePerson);
+
+            File.WriteAllText("person.json", jsonString);
+
+            Console.WriteLine("JSON serialization complete");
         }
     }
 }
