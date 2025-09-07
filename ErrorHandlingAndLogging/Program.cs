f41648525debdb48f82eea58ffff8c80e343ab76
Damian Zabagło <damian.zabaglo@microsoft.wsei.edu.pl>
2025-09-07 15:47:03 +0200

    Add Global Exception handling, add Serilog, configure serilog

--- ErrorHandlingAndLogging/Program.cs
+++ ErrorHandlingAndLogging/Program.cs
@@ -1,10 +1,33 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Services.AddControllers();
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 
 var app = builder.Build();
+app.Use(async (context, next) =>
+{
+    try
+    {
+        await next();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Global exception caught: {ex}");
+        context.Response.StatusCode = 500;
+        await context.Response.WriteAsync("An unexpected error occured. Try again later.");
+    }
+});
 
 
+app.UseRouting();
+app.MapControllers();
 app.Run();
 
