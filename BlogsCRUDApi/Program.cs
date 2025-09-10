28ebb58f70db6c45ce583c2ced5eec40ca26b012
Damian Zabagło <damian.zabaglo@microsoft.wsei.edu.pl>
2025-09-10 20:30:27 +0200

    Add enpoints tests

--- BlogsCRUDApi/Program.cs
+++ BlogsCRUDApi/Program.cs
@@ -2,8 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 var blogs = new List<Blog>
 {
-    new Blog { Title = "My First Post", Body = "This is my first post" }
-    new Blog { Title = "My Second Post", Body = "This is my second post" }
+    new Blog { Title = "My First Post", Body = "This is my first post" },
+    new Blog { Title = "My Second Post", Body = "This is my second post" },
 };
 
 app.MapGet("/", () => "I am root!");
